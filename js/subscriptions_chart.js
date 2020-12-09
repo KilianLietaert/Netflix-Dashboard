56542d3dff9d89a366e4fc85aa0452707393d50b
Michel Van Autrève <michel.vanautreve@outlook.com>
2020-12-09 10:57:29 +0100

    Data wordt juist weergegeven

--- js/subscriptions_chart.js
+++ js/subscriptions_chart.js
@@ -1,25 +1,41 @@
-new Chartist.Bar('.ct-chart', {
-  labels: ['Basic', 'Standard', 'Premium'],
-  series: [
-    [{value: 2000000, className: 'foo'}, {value: 18000000, className: 'bar'}, {value: 11000000, className: 'bar'}],
-      [{value: 17000000, className: 'foo'}, {value: 44500000, className: 'bar'}, {value: 35000000, className: 'bar'}]
-  ]
-}, {
-  seriesBarDistance: 10,
-  axisX: {
-    offset: 60
-  },
-  axisY: {
-    offset: 80,
-    labelInterpolationFnc: function(value) {
-      return (value /100000)
-    },
-    scaleMinSpace: 15
-  }
+const init = function(){
+  var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: ['Basic', 'Standard', 'Premium'],
+            datasets: [{
+                label: 'Subscribed users',
+                backgroundColor: '#e21221',
+                data: [17000000, 44500000, 35000000],
+                barThickness: 0.5
+            },{
+                label: 'Trial users',
+                backgroundColor: 'white',
+                data: [2000000, 18000000, 11500000],
+                
+            },]
+        },
+
+        // Configuration options go here
+        options: {
+            legend: {
+            labels: {
+                // This more specific font property overrides the global property
+                fontColor: 'white'
+            }
+        }
+        }
 });
-  
-  var options = {
-    seriesBarDistance: 15
-  };
-  
-  new Chartist.Bar('.ct-chart', data, options);
+
+Chart.scaleService.updateScaleDefaults('linear', {
+    ticks: {
+        min: 12000000
+    }
+});
+}
+
+document.addEventListener("DOMContentLoaded", init);
